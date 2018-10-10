91453435fbd7d6b77e08356c4b0834e03b2edb90
BenWu <hidesor@yahoo.com.tw>
2018-10-10 23:24:32 +0800

    顯示房屋資料

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,16 +1,20 @@
 class HousesController < ApplicationController
 
   def index
+    house_data = House.house_data
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: "Population vs GDP For 5 Big Countries [2009]")
-      f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
+      # f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
+      f.xAxis(categories: house_data[0])
       # 若要顯示一列可僅顯示一行
-      f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
-      f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
+      # f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
+      # f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
+      f.series(name: "Square Feet AVG", yAxis: 0, data: house_data[1])
+      f.series(name: "Total Price AVG", yAxis: 1, data: house_data[2])
 
       f.yAxis [
-        {title: {text: "GDP in Billions", margin: 70} },
-        {title: {text: "Population in Millions"}, opposite: true},
+        {title: {text: "Square Feet AVG", margin: 70} },
+        {title: {text: "Total Price AVG"}, opposite: true},
       ]
 
       f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,7 @@ class House < ApplicationRecord
   # https://data.gov.tw/dataset/26820
   # http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000221-001
 
+  # 取回 house的資料存入db
   def self.get_house
     require 'net/http'
     source = 'http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000221-001'
@@ -21,6 +22,26 @@ class House < ApplicationRecord
         total_price: house["rps21"]
       )
     end
-
   end
+
+  # 整理資料
+  def self.house_data
+    areas = House.select("area").group("area")
+    square_feet_arr = []
+    total_price_arr = []
+    areas_arr = []
+    areas.each do |area|
+      square_feet_data = House.where(area: area.area).map{ |m| m.square_feet.to_f }
+      square_feet_avg = square_feet_data.sum/square_feet_data.size
+      total_price_data = House.where(area: area.area).map{ |m| m.total_price.to_f }
+      total_price_avg = total_price_data.sum/total_price_data.size
+      if square_feet_avg < 150
+        areas_arr << area.area
+        square_feet_arr << square_feet_avg
+        total_price_arr << (total_price_avg/10000)
+      end
+    end
+    return [areas_arr, square_feet_arr, total_price_arr]
+  end
+
 end
