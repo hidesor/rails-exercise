d96cf548900572dee5196a184c85dbf0c2d7a2ca
BenWu <hidesor@yahoo.com.tw>
2018-10-21 23:19:57 +0800

    宣告可數字,已按數字為Array, 按下按鈕後可變更內容

--- app/assets/javascripts/oxo.js
+++ app/assets/javascripts/oxo.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
-  $('.ox_btn').click(function(){
-     //id = $(this)[0].id.substring(4,6);
-     console.log($(this)[0].id.substring(2,3));
-     $(this).html('O')
+
+    var can_push_arr = [];
+    var user_push_arr = [];
+    var npc_push_arr = []
+
+    var restart = function(){
+        can_push_arr = [1,2,3,4,5,6,7,8,9];
+        user_push_arr = [];
+        npc_push_arr = [];
+    }
+    restart();
+
+
+    // 使用者按下按鈕後動作
+    $('.ox_btn').click(function(){
+        console.log($(this)[0].id.substring(2,3));
+        id = $(this)[0].id.substring(2,3);
+
+        user_push_arr.push(Number(id));
+
+        can_push_arr = jQuery.grep(can_push_arr, function(value) {
+            return value != id;
+        });
+        console.log('can_push_arr',can_push_arr);
+        console.log('user_push_arr',user_push_arr);
+
+        $(this).html('O')
+
+
      // word_id = 'word_' + id;
      // word = $('#'+word_id).html();
      // url = "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=" + word + "&tl=en-gb";
