f4221927532a54f4a3619732b66ca39263b3ff0a
BenWu <hidesor@yahoo.com.tw>
2018-10-22 22:58:33 +0800

    npc第一步會下到較好的位置

--- app/assets/javascripts/oxo.js
+++ app/assets/javascripts/oxo.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    var npc_first_arr = [1,3,5,7,9];
+    var success_arr = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+
     var can_push_arr = [];
     var user_push_arr = [];
     var npc_push_arr = []
@@ -11,9 +14,25 @@ $(document).ready(function(){
     }
     restart();
 
+    function randomSort(a, b) {
+      return Math.random() > 0.5 ? -1 : 1;
+    }
+
     var play_by_npc = function(){
       // npc 隨機下一格
-      npc_do_id = can_push_arr[Math.floor(Math.random()*can_push_arr.length)];
+      // npc_do_id = can_push_arr[Math.floor(Math.random()*can_push_arr.length)];
+      npc_do_id = 0
+      if(npc_push_arr.length == 0){
+          $.each(npc_first_arr.sort(randomSort), function(index, value ) {
+              if(user_push_arr.indexOf(Number(value)) == -1 ){
+                  npc_do_id = value;
+                  return false;
+              }
+          })
+      }else{
+
+      }
+
       console.log('npc_do_id',npc_do_id);
 
       npc_push_arr.push(Number(npc_do_id));
