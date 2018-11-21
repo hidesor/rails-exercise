4d9d17aa015c371af3906c04d28673cd84a29018
BenWu <hidesor@yahoo.com.tw>
2018-11-21 23:19:15 +0800

    bootstrap test

--- config/routes.rb
+++ config/routes.rb
@@ -37,15 +37,17 @@ Rails.application.routes.draw do
   end
 
   get "/demo" => "welcome#index"
-  get "/welcome/word" => "welcome#word"
+  get "/home" => "home#index"
+  get "/welcomes/word" => "welcome#word"
   get "/something" => "welcome#something"
   get "/oxos" => "oxos#index"
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcomes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
