d0cf37a84cd883e0ab03bb97e84508ee31f1dc12
BenWu <hidesor@yahoo.com.tw>
2019-03-04 23:31:12 +0800

    調整頁面樣式

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -21,7 +21,7 @@ class Admin::WordsController < ApplicationController
 
     respond_to do |format|
       if @word.save
-        format.html { redirect_to edit_admin_word_path(@word), notice: '資料建立成功' }
+        format.html { redirect_to admin_words_path, notice: '資料建立成功' }
       else
         format.html { render :new }
       end
