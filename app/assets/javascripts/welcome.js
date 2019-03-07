284c799c0f7a8e2b01179e4fa4f86dc7bdfcdfe5
BenWu <hidesor@yahoo.com.tw>
2019-03-07 23:41:42 +0800

    按下圖示可標示結果

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
      url = "https://translate.google.com.tw/?hl=zh-TW#view=home&op=translate&sl=en&tl=zh-TW&text="+word
      window.open(url);
   });
+
+  $(".is_know").click(function(){
+      console.log('is_know');
+      console.log($(this)[0])
+      var class_name = $(this)[0].children[0].className
+      console.log('class_name', class_name);
+      if(class_name == 'fa fa-question'){
+        $(this)[0].children[0].outerHTML = '<i class="fab fa-angellist"></i>'
+      }else if(class_name == 'fab fa-angellist'){
+        $(this)[0].children[0].outerHTML = '<i class="fa fa-thumbs-down" aria-hidden="true"></i>'
+      }else{
+        $(this)[0].children[0].outerHTML = '<i class="fab fa-angellist"></i>'
+      }
+  })
+
 });
+
+// <i class="fab fa-angellist"></i>
+// <i class="fa fa-thumbs-down" aria-hidden="true"></i>
