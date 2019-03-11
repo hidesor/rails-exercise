412212f959a49a433da7f8da69b1fce49509dbf4
BenWu <hidesor@yahoo.com.tw>
2019-03-11 23:48:15 +0800

    以往的單字可進行reset

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
      window.open(url);
   });
 
-  $(".is_know").click(function(){
+  $(".is_know_week").click(function(){
       console.log('is_know');
       console.log($(this)[0])
       var class_name = $(this)[0].children[0].className
@@ -22,12 +22,35 @@ $(document).ready(function(){
       }
   })
 
+
+    $(".is_know_before").click(function(){
+        console.log('is_know');
+        console.log($(this)[0])
+        var class_name = $(this)[0].children[0].className
+        console.log('class_name', class_name);
+        if(class_name == 'fa fa-question'){
+          $(this)[0].children[0].outerHTML = '<i class="fab fa-angellist"></i>'
+        }else if(class_name == 'fab fa-angellist'){
+          $(this)[0].children[0].outerHTML = '<i class="fa fa-thumbs-down" aria-hidden="true"></i>'
+        }else{
+          $(this)[0].children[0].outerHTML = '<i class="fab fa-angellist"></i>'
+        }
+    })
+
   $("#reset_this_week").click(function(){
-      console.log('week');
-      $(".is_know").each(function(index){
-           $(this)[0].children[0].outerHTML = '<i class="fa fa-question" aria-hidden="true"></i>'; 
+      $(".is_know_week").each(function(index){
+           $(this)[0].children[0].outerHTML = '<i class="fa fa-question" aria-hidden="true"></i>';
       })
   })
+
+  $("#reset_this_before").click(function(){
+      console.log('week');
+      $(".is_know_before").each(function(index){
+           $(this)[0].children[0].outerHTML = '<i class="fa fa-question" aria-hidden="true"></i>';
+      })
+  })
+
+
 });
 
 // <i class="fab fa-angellist"></i>
