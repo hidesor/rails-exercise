08c15f75a5f11231a27b8bde17585e65d2fa5ad7
BenWu <hidesor@yahoo.com.tw>
2019-03-11 23:13:25 +0800

    首頁最開始僅檢視最新7天的單字

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -3,7 +3,8 @@ class WelcomesController < ApplicationController
   def index
     # @words = Word.select("times").group("times").order(times: :asc)
     # Order.select("store_name, sum(sales)").group("store")
-    @words = Word.all.order(id: :desc)
+    @last_word = Word.all.order(created_at: :asc).last
+    @words = Word.where('created_at > ?', @last_word.created_at - 7.days).order(id: :desc)
   end
 
   def word
