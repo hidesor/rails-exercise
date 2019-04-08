782eefabb8f9663f1e6cc3c2c362c3b3db12dd13
BenWu <hidesor@yahoo.com.tw>
2019-04-08 23:44:11 +0800

    js可取得難度

--- app/assets/javascripts/arithmetic.js
+++ app/assets/javascripts/arithmetic.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
         $("input[name='arithmetic[]']:checked").each( function () { 
             arithmetic.push($(this).val());
         })
+
+        var lv = $('input:radio:checked[name="lv"]').val();
         console.log('arithmetic',arithmetic)
+        console.log('lv',lv)
    }) 
 });
 
