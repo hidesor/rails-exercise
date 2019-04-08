f2c6c30ad9782c075192308eb153fb92fd465625
BenWu <hidesor@yahoo.com.tw>
2019-04-08 23:32:24 +0800

    js可取得加減乘除

--- app/assets/javascripts/arithmetic.js
+++ app/assets/javascripts/arithmetic.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){ 
-   $('#test_start').on('click',function(){
-       console.log('test') 
+   $('#test_start').on('click',function(){ 
+        var arithmetic = [];
+        $("input[name='arithmetic[]']:checked").each( function () { 
+            arithmetic.push($(this).val());
+        })
+        console.log('arithmetic',arithmetic)
    }) 
-
 });
+
