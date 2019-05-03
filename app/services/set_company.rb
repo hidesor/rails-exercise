4e99a1ae602c09394104e7716c095f6619e378e7
BenWu <hidesor@yahoo.com.tw>
2019-05-03 15:32:30 +0800

    可驗證統編是否已被使用

--- app/services/set_company.rb
+++ app/services/set_company.rb
@@ -1,16 +1,19 @@
-class Company
+class SetCompany
   def initialize(gui_number)
     @gui_number = gui_number
   end
 
   # 產生統編
   def create
-    check
-    binding.pry
+    if exist? && unused?
+      binding.pry 
+    else
+      binding.pry 
+    end
   end
 
-  # 驗證統編是否正確
-  def check
+  # 驗證統編存在
+  def exist?
     require 'rest-client'
     url = "http://data.gcis.nat.gov.tw/od/data/api/5F64D864-61CB-4D0D-8AD9-492047CC1EA6?$format=json&$filter=Business_Accounting_NO%20eq%20"+@gui_number+"&$skip=0&$top=50" 
     response = RestClient.get(url)
@@ -18,4 +21,9 @@ class Company
     (response_data.count > 0 && response_data[0]["Business_Accounting_NO"] == @gui_number)? true : false
   end
 
+  # 驗證統編在此系統未被使用
+  def unused?
+    company = Company.find_by(gui_number: @gui_number)
+    company.blank?? true : false 
+  end
 end
