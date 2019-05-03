a3382c1cc200a2f0edb7ac0b68fa46815e90b1ef
BenWu <hidesor@yahoo.com.tw>
2019-05-03 16:31:14 +0800

    建立公司時, 統編符合條件才可建立

--- app/services/set_company.rb
+++ app/services/set_company.rb
@@ -6,9 +6,8 @@ class SetCompany
   # 產生統編
   def create
     if exist? && unused?
-      binding.pry 
-    else
-      binding.pry 
+      company = Company.create(name: @response_data[0]['Company_Name'], gui_number: @gui_number) 
+      return company
     end
   end
 
@@ -17,8 +16,8 @@ class SetCompany
     require 'rest-client'
     url = "http://data.gcis.nat.gov.tw/od/data/api/5F64D864-61CB-4D0D-8AD9-492047CC1EA6?$format=json&$filter=Business_Accounting_NO%20eq%20"+@gui_number+"&$skip=0&$top=50" 
     response = RestClient.get(url)
-    (response == "")? (return false) : (response_data = JSON.parse(response.body))
-    (response_data.count > 0 && response_data[0]["Business_Accounting_NO"] == @gui_number)? true : false
+    (response == "")? (return false) : (@response_data = JSON.parse(response.body))
+    (@response_data.count > 0 && @response_data[0]["Business_Accounting_NO"] == @gui_number)? true : false
   end
 
   # 驗證統編在此系統未被使用
