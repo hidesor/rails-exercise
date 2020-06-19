6f78400ab92fca35c9fb52e8fd841d6305c16ef5
BenWu <benwu04@larvata.tw>
2020-06-19 22:14:11 +0800

    拿掉graphql

--- app/graphql/schema.rb
+++
@@ -1,9 +0,0 @@
-require "apollo/tracing"
-
-class Schema < GraphQL::Schema
-  query Types::QueryType
-  mutation Mutations::MutationType
-
-  use BatchLoader::GraphQL
-  # use ApolloTracing.new
-end
